291065b4f194471be23f0934672203eaf8f01e12
Matheus Victor <matheusvictor.salles@gmail.com>
2022-07-20 15:58:15 -0300

    Add do repositório
    Exemplo adicionado pelo professor. Esse é um repositório de estudos!

--- ParametrosValorRefFuncaoPorPrototipo.cpp
+++ ParametrosValorRefFuncaoPorPrototipo.cpp
@@ -7,85 +7,14 @@
 int g0 = 10,
 	g1 = 30;
 
-/* ************************************************************ */ 
-/* * Passagem de parametros por valor para os dois parametros * */ 
-/* * Efeito colateral para uma das variáveis globais          * */ 
-/* ************************************************************ */ 
+/**
+ * Definção das funções por prótipos 
+*/
 
-void F0(int a, int b) { 
-
-	std::cout << "##### F0 #####" << std::endl; 
-
-	a += 10;
-	b *= 2;
-
-	std::cout << "	 a = " << a << " [ " << &a << " ] " << std::endl;
-	std::cout << "	 b = " << b << " [ " << &b << " ] " << std::endl;
-
-	g0 = a * b;
-
-}
-
-/* ************************************************************ */ 
-/* * Passagem por referencia para um dos parametros           * */
-/* * Uso de variável local com mesmo nome da global,          * */ 
-/* *   sem efeito colateral                                   * */ 
-/* ************************************************************ */ 
-
-void F1(int &a, int b) {
-
-int g0 = 0; 
-
-	std::cout << "##### F1 #####" << std::endl; 
-
-	a += 10;
-	b *= 2;
-
-	std::cout << "	 a  = " << a << " [ " << &a << " ] " << std::endl;
-	std::cout << "	 b  = " << b << " [ " << &b << " ] " << std::endl;
-	std::cout << "	 g0 = " << g0 << " [ " << &g0 << " ] " << std::endl;
-
-	g0 = a * b;
-
-}
-
-/* ************************************************************ */ 
-/* * Passagem por referencia para os dois parametros.         * */
-/* * Uso de variável local com mesmo nome da global,          * */ 
-/* *   sem efeito colateral (escopo local prevalece)          * */ 
-/* ************************************************************ */ 
-
-void F2(int &g0, int &g1) {
-
-	std::cout << "##### F2 #####" << std::endl; 
-
-	g0 += 10;
-	g1 *= 2;
-	
-	std::cout << "	g0 = " << g0 << " [ " << &g0 << " ] " << std::endl;
-	std::cout << "	g1 = " << g1 << " [ " << &g1 << " ] " << std::endl;
-
-	g0 = g0 * g1;
-}
-
-/* ************************************************************ */ 
-/* * Passagem por referencia para os dois parametros.         * */
-/* * Uso de variável local com mesmo nome da global,          * */ 
-/* *   sem efeito colateral                                   * */ 
-/* ************************************************************ */ 
-
-void F3(int *g0, int *g1) {
-
-	std::cout << "##### F3 #####" << std::endl; 
-
-	*g0 += 10;
-	*g1 *= 2;
-	
-	std::cout << "	g0 = " << *g0 << " [ " << g0 << " ] " << std::endl;
-	std::cout << "	g1 = " << *g1 << " [ " << g1 << " ] " << std::endl;
-
-	*g0 = (*g0) * (*g1);
-}
+void F0(int a, int b);
+void F1(int &a, int b);
+void F2(int &a, int &b);
+void F3(int *a, int *b);
 
 /* ************************************************************ */ 
 /* ************************************************************ */ 
@@ -173,4 +102,84 @@ int m_loc0 = 55,
 	std::cout << "*************************" << std::endl; 
 
 	return 0;
+}
+
+/* ************************************************************ */ 
+/* * Passagem de parametros por valor para os dois parametros * */ 
+/* * Efeito colateral para uma das variáveis globais          * */ 
+/* ************************************************************ */ 
+
+void F0(int a, int b) { 
+
+	std::cout << "##### F0 #####" << std::endl; 
+
+	a += 10;
+	b *= 2;
+
+	std::cout << "	 a = " << a << " [ " << &a << " ] " << std::endl;
+	std::cout << "	 b = " << b << " [ " << &b << " ] " << std::endl;
+
+	g0 = a * b;
+
+}
+
+/* ************************************************************ */ 
+/* * Passagem por referencia para um dos parametros           * */
+/* * Uso de variável local com mesmo nome da global,          * */ 
+/* *   sem efeito colateral                                   * */ 
+/* ************************************************************ */ 
+
+void F1(int &a, int b) {
+
+int g0 = 0; 
+
+	std::cout << "##### F1 #####" << std::endl; 
+
+	a += 10;
+	b *= 2;
+
+	std::cout << "	 a  = " << a << " [ " << &a << " ] " << std::endl;
+	std::cout << "	 b  = " << b << " [ " << &b << " ] " << std::endl;
+	std::cout << "	 g0 = " << g0 << " [ " << &g0 << " ] " << std::endl;
+
+	g0 = a * b;
+
+}
+
+/* ************************************************************ */ 
+/* * Passagem por referencia para os dois parametros.         * */
+/* * Uso de variável local com mesmo nome da global,          * */ 
+/* *   sem efeito colateral (escopo local prevalece)          * */ 
+/* ************************************************************ */ 
+
+void F2(int &g0, int &g1) {
+
+	std::cout << "##### F2 #####" << std::endl; 
+
+	g0 += 10;
+	g1 *= 2;
+	
+	std::cout << "	g0 = " << g0 << " [ " << &g0 << " ] " << std::endl;
+	std::cout << "	g1 = " << g1 << " [ " << &g1 << " ] " << std::endl;
+
+	g0 = g0 * g1;
+}
+
+/* ************************************************************ */ 
+/* * Passagem por referencia para os dois parametros.         * */
+/* * Uso de variável local com mesmo nome da global,          * */ 
+/* *   sem efeito colateral                                   * */ 
+/* ************************************************************ */ 
+
+void F3(int *g0, int *g1) {
+
+	std::cout << "##### F3 #####" << std::endl; 
+
+	*g0 += 10;
+	*g1 *= 2;
+	
+	std::cout << "	g0 = " << *g0 << " [ " << g0 << " ] " << std::endl;
+	std::cout << "	g1 = " << *g1 << " [ " << g1 << " ] " << std::endl;
+
+	*g0 = (*g0) * (*g1);
 }
